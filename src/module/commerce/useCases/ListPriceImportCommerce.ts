231a3d0407cd2e2bbd31dd0ba3281e75b41f2a94
MatheusThiesen <matheusreis314@gmail.com>
2023-02-09 14:42:49 -0300

    lista de preco

--- src/module/commerce/useCases/ListPriceImportCommerce.ts
+++ src/module/commerce/useCases/ListPriceImportCommerce.ts
@@ -1,3 +1,4 @@
+import { ItemPriceList } from "src/module/entities/model/ItemPriceList";
 import { ItemPriceListRepository } from "../../entities/repositories/ItempriceListRepository";
 import { SendDataRepository } from "../repositories/SendDataRepository";
 import { ExecuteServiceProps } from "../types/ExecuteService";
@@ -12,15 +13,30 @@ export interface ItemPriceNormalized {
 }
 
 export class ListPriceImportCommerce {
+  readonly size = 800;
+
   constructor(
     private sendData: SendDataRepository,
     private itemPriceListRepository: ItemPriceListRepository
   ) {}
 
+  getNormalizedDate(items: ItemPriceList[]): ItemPriceNormalized[] {
+    const normalized = items.map((item) => ({
+      id: String(item.identifier),
+      codigo: item.priceList.code,
+      descricao: item.priceList.description,
+      valor: item.priceRange[0].salePrice,
+      situation: item.priceList.situation,
+      produtoCodigo: item.product.code,
+    }));
+
+    return normalized;
+  }
+
   async execute({ search }: ExecuteServiceProps) {
     const query = search;
 
-    const products = await this.itemPriceListRepository.getAll({
+    const itemsPrice = await this.itemPriceListRepository.getAll({
       fields: {
         identifier: true,
         priceList: {
@@ -37,19 +53,50 @@ export class ListPriceImportCommerce {
       },
 
       search: query,
+
+      isPagination: true,
+      page: 0,
+      size: this.size,
     });
 
-    const itemPriceNormalized: ItemPriceNormalized[] = products.content.map(
-      (item) => ({
-        id: String(item.identifier),
-        codigo: item.priceList.code,
-        descricao: item.priceList.description,
-        valor: item.priceRange[0].salePrice,
-        situation: item.priceList.situation,
-        produtoCodigo: item.product.code,
-      })
+    await this.sendData.post(
+      "/price-lists/import",
+      this.getNormalizedDate(itemsPrice.content)
     );
 
-    await this.sendData.post("/price-lists/import", itemPriceNormalized);
+    const totalPages = Number(itemsPrice.totalPages);
+
+    for (let index = 0; index < totalPages; index++) {
+      const page = index + 1;
+
+      console.log(`Lists price  ${page} de ${totalPages}`);
+
+      const listsItemsResponse = await this.itemPriceListRepository.getAll({
+        fields: {
+          identifier: true,
+          priceList: {
+            code: true,
+            description: true,
+            situation: true,
+          },
+          product: {
+            code: true,
+          },
+          priceRange: {
+            salePrice: true,
+          },
+        },
+
+        search,
+        isPagination: true,
+        page: page,
+        size: this.size,
+      });
+
+      await this.sendData.post(
+        "/price-lists/import",
+        this.getNormalizedDate(listsItemsResponse.content)
+      );
+    }
   }
 }
