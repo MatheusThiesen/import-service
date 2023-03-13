d9f71706fef9b051679a436652fc58aeaa64c457
MatheusThiesen <matheusreis314@gmail.com>
2023-03-13 13:43:06 -0300

    bug da finalizacao do db

--- src/service/dbSiger.ts
+++ src/service/dbSiger.ts
@@ -1,34 +1,25 @@
 import * as mariadb from "mariadb";
 
 class DbSiger {
-  private pool = mariadb.createPool({
-    host: process.env.RECH_MARIADB_HOST,
-    port: Number(process.env.RECH_MARIADB_PORT),
-    user: process.env.RECH_MARIADB_USER,
-    password: process.env.RECH_MARIADB_PASS,
-    connectionLimit: 15,
-  });
-
-  private connection: any;
-
-  private async openConnection() {
-    this.connection = await this.pool.getConnection();
-  }
-  private async closeConnection() {
-    this.connection.end();
-  }
-
   public async $ExecuteQuery<T>(query: string): Promise<T[]> {
-    try {
-      await this.openConnection();
-      const rows = await this.connection.query(query);
+    const pool = mariadb.createPool({
+      host: process.env.RECH_MARIADB_HOST,
+      port: Number(process.env.RECH_MARIADB_PORT),
+      user: process.env.RECH_MARIADB_USER,
+      password: process.env.RECH_MARIADB_PASS,
+      connectionLimit: 15,
+    });
 
+    const connection = await pool.getConnection();
+    const rows = await connection.query(query);
+
+    try {
       delete rows.meta;
       return rows;
     } catch (err) {
       throw err;
     } finally {
-      await this.closeConnection();
+      await connection.end();
     }
   }
 }
